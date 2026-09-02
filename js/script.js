affdfa1a6362fbae11b4117151aadc8e9568a9a7
Calvin Roberts <roberts.calvin85@gmail.com>
2026-09-02 22:53:15 +0100

    Add javascript to parks page

--- js/script.js
+++ js/script.js
@@ -12,3 +12,22 @@ function pickImage() {
   img.setAttribute("src", randomImg);
   img.setAttribute("alt", "");
 }
+
+function setImages() {
+  options = [
+    "bacon_in_the_woods.png",
+    "bend_in_the_road.png",
+    "hollow_tree.png",
+    "lake_view.JPG",
+    "hanging_plants.png",
+    "munising.jpg",
+  ];
+  currentImages = document.querySelectorAll(".flex img");
+  for (i = 0; i < currentImages.length; i++) {
+    console.log("Image " + i);
+
+    randomImg = "images/" + options[Math.floor(Math.random() * options.length)];
+    currentImages[i].setAttribute("src", randomImg);
+    currentImages[i].setAttribute("tabindex", "0");
+  }
+}
